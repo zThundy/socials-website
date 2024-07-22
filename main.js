c1a3e77c49b90acea0d3277a77855e53ff3e68c1
zThundy <antonioantonelli7@gmail.com>
2024-07-22 13:47:13 +0200

    added some projects

--- main.js
+++ main.js
@@ -33,9 +33,9 @@ window.onload = function () {
         }
 
         // scale out animation with ease-out effect using scale()
-        for (let i = 1.2; i >= 1; i -= 0.05) {
+        for (let i = 1.3; i >= 1; i -= 0.05) {
             modalConent.style.transform = `scale(${i})`;
-            await new Promise(resolve => setTimeout(resolve, 5));
+            await new Promise(resolve => setTimeout(resolve, 10));
         }
     }
 
@@ -50,5 +50,38 @@ window.onload = function () {
         modal.style.display = "none";
     }
 
-    openModal("Welcome", "This is a simple modal window created using vanilla JavaScript. Click the close button to close the modal window.");
+    const projects = {
+        "project1": [
+            "https://github.com/echo-project-org",
+            false
+        ],
+        "project2": [
+            "https://github.com/zThundy/clipper",
+            "https://clipmaner.com/",
+        ],
+        "project3": [
+            "https://github.com/zThundy/Guess-the-song-SSR",
+            false
+        ],
+        "project4": [
+            "https://github.com/zThundy/src_htmlPhone",
+            "https://fivem.net/"
+        ]
+    }
+
+    const pbtn = document.getElementsByClassName("pbtn");
+    for (let i = 0; i < pbtn.length; i++) {
+        pbtn[i].addEventListener("click", function (e) {
+            const data = e.target.dataset;
+            if (projects[data.id] && projects[data.id][data.type]) {
+                console.log(projects[data.id][data.type]);
+                data.type = Number(data.type);
+                window.open(projects[data.id][data.type], "_blank");
+            } else {
+                console.warn("Project not found");
+            }
+        });
+    }
+
+    // openModal("Welcome", "This is a simple modal window created using vanilla JavaScript. Click the close button to close the modal window.");
 }
