1993c49e3c37c530e8ad80f7f15ec2ab5c211426
zThundy <antonioantonelli7@gmail.com>
2024-07-24 16:30:57 +0200

    added jquery and some edits to html

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 window.onload = function () {
-    const elements = document.getElementsByClassName("sidebar-click");
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].addEventListener("click", function (e) {
-            try {
-                // get data from dataset parent
-                const data = e.target.parentElement.dataset;
-                // scroll into view using data.id
-                const element = document.getElementById(data.id);
-                element.scrollIntoView({ behavior: "smooth", block: "start" });
-            } catch (e) {
-                console.warn("Element not found");
-            }
-        });
-    }
+    // get all elements with class "sidebar-click" with jquery
+    $(".sidebar-click").click(function (e) {
+        try {
+            // get data from dataset parent
+            const data = e.target.parentElement.dataset;
+            // scroll into view using data.id
+            const element = document.getElementById(data.id);
+            element.scrollIntoView({ behavior: "smooth", block: "start" });
+        } catch (e) {
+            console.warn("Element not found");
+        }
+    });
 
     const modal = document.getElementById("modal");
     const modalTitle = document.getElementById("modal-title");
